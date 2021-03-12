841e77d2aedaf729a69b660e0f9d9ed9d2c89215
Carter Culkin <carter.culkin@gmail.com>
2021-03-12 15:21:23 -0600

    fixed

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ const filterLegos = (whatFilter) => {
 }
 
 const legosByMaterial = (whatMaterial) => {
-	const materialArray = use().filter(singleLego => {
+	const materialArray = useLegos().filter(singleLego => {
 		if (singleLego.Material === whatMaterial) {
 			return singleLego;
 		}
@@ -53,3 +53,25 @@ const startEIA = () => {
 }
 
 startEIA();
+
+navElement.addEventListener("keyup", (event) => {
+	if (event.key === "Enter"){
+		if (event.target.id === "search"){
+			searchBar;
+		}
+	}
+})
+
+function searchBar() {
+	const legoID = document.getElementById("search").value
+	console.log("legoID", legoID)
+	const legoIDSearch = useLegos().filter(singleLego => {
+		if(singleLego.legoId === legoID){
+			return singleLego
+		}
+	})
+	if (legoIDSearch.length === 0){
+		document.getElementById("all-legos").innerHTML = "<p> Sadly we couldn't find what you are looking for </p>"
+	}
+	else { makeLegoList(legoIDSearch)}
+}
